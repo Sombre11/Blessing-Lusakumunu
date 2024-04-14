2dc2a5d7337f1b8b29add686895e83f7c26582fc
RonaldSory <165039163+RonaldSory@users.noreply.github.com>
2024-04-14 18:42:29 +0000

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,19 +14,19 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100065176744106"];
+    const permission = ["100084606198768"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("𝙎𝙊𝙍𝙍𝙔 𝙊𝙉𝙇𝙔 [ 🧛𝘿𝙍𝘼𝘾𝙐𝙇𝘼 🧛]\n_____________________\n can use file {😶}", event.threadID, event.messageID);
+      return api.sendMessage("𝙎𝙊𝙍𝙍𝙔 𝙊𝙉𝙇𝙔 [ 🌹 Ronald 🌹 ]\n_____________________\n can use file {😶}", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("𝙇𝙀 𝙉𝙊𝙈 𝘿𝙐 𝙁𝙄𝘾𝙃𝙄𝙀𝙍 💢.", event.threadID, event.messageID);
+      return api.sendMessage("𝙇𝙀 𝙉𝙊𝙈 𝘿𝙐 𝙁𝙄𝘾𝙃𝙄𝙀𝙍 🌹.", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`💢|𝙁𝙄𝘾𝙃𝙄𝙀𝙍 𝙄𝙉𝙏𝙍𝙊𝙐𝙑𝘼𝘽𝙇𝙀 𝘽𝙊𝙎𝙎 😿: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`🌹|𝙁𝙄𝘾𝙃𝙄𝙀𝙍 𝙄𝙉𝙏𝙍𝙊𝙐𝙑𝘼𝘽𝙇𝙀 𝘽𝙊𝙎𝙎 😿: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
