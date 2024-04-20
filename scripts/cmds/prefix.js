bb675432d5a1929df7215e1e8a6bb434a6095048
SoryRonald <165383634+SoryRonald@users.noreply.github.com>
2024-04-20 13:51:13 +0000

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌹 𝐑𝐨𝐧𝐚𝐥𝐝 🌹\n\n  𝐕𝐎𝐈𝐂𝐈 𝐌𝐎𝐍 𝐏𝐑𝐄𝐅𝐈𝐗  ☛ %1\n_______________________\n 🌹"
+			myPrefix: "🌹 𝐑𝐨𝐧𝐚𝐥𝐝 🌹\n══════════════════\n  𝐕𝐎𝐈𝐂𝐈 𝐌𝐎𝐍 𝐏𝐑𝐄𝐅𝐈𝐗 ☛ %1\n══════════════════\n Alors écrit ☟\n %1 help  "
 		}
 	},
 
